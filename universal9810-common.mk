e214b95b094835f97462c48f0d4eff1e88c96409
Lunarixus <Nathan@lunarixus.party>
2020-11-17 15:32:40 +0000

    universal9810: Add HIDL manifest
    Signed-off-by: Lunarixus <Nathan@lunarixus.party>

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -111,6 +111,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
     android.hardware.health@2.0-service
 
+# HIDL Manifest
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/manifest.xml:$(TARGET_COPY_OUT_VENDOR)/manifest.xml
+
 # SP-NDK
 PRODUCT_PACKAGES += \
     libvulkan
