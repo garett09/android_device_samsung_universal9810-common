afa16be74f40befdd2ca7bcc8933528612534089
Lunarixus <Nathan@lunarixus.party>
2020-11-17 15:20:43 +0000

    universal9810: Build camera packages
    Signed-off-by: Lunarixus <Nathan@lunarixus.party>

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -59,6 +59,18 @@ TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 TARGET_SCREEN_HEIGHT := 2960
 TARGET_SCREEN_WIDTH := 1440
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.common@1.0 \
+    android.hardware.camera.device@1.0 \
+    android.hardware.camera.device@3.2 \
+    android.hardware.camera.device@3.3 \
+    android.hardware.camera.device@3.4 \
+    android.hardware.camera.provider@2.4 \
+    Snap
+
 # SP-NDK
 PRODUCT_PACKAGES += \
     libvulkan
