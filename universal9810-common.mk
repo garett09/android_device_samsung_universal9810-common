6b94760ca1dc3451f009eec317f622d21bbb266a
Lunarixus <Nathan@lunarixus.party>
2020-11-17 15:38:38 +0000

    universal9810: Build memtrack packages
    Signed-off-by: Lunarixus <Nathan@lunarixus.party>

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -161,6 +161,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Memtrack
+PRODUCT_PACKAGES += \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
+
 # Net
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
