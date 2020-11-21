5a9aff80b631ca23f44fd0b12e2bbe1083f0b7e9
Adrian Sian <adriansian@gmail.com>
2020-11-22 01:21:29 +0800

    universal9810: update props for galaxy wearable
    Signed-off-by: Adrian Sian <adriansian@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -58,3 +58,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-cpu-set=0,1,2,3,4,5,6,7
     dalvik.vm.boot-dex2oat-threads=8 \
     dalvik.vm.boot-dex2oat-cpu-set=0,1,2,3,4,5,6,7
+
+# overrides
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.product.brand=google \
+    ro.product.device=coral \
+    ro.product.manufacturer=google \
+    ro.product.model=Pixel 4 XL \
+    ro.product.name=coral \
+    ro.vendor.product.manufacturer=google
