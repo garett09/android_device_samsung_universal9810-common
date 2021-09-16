5c0d5b89747367cf4fc9454a07904ac9b18cf545
synt4x93 <synt4x.93@gmail.com>
2021-09-16 13:10:52 +0100

    universal9810: recovery: specify BOARD_HAS_DOWNLOAD_MODE
    Change-Id: If3602603eb10caa7e134885a98ee59e41afa9654

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -74,6 +74,7 @@ BOARD_ROOT_EXTRA_FOLDERS := efs
 # Recovery
 TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.samsungexynos9810.recovery
+BOARD_HAS_DOWNLOAD_MODE := true
 
 # Samsung HALs
 TARGET_POWERHAL_VARIANT := samsung
