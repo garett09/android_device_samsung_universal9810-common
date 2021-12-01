24caec6aa5e70f767ad644f3d2e1f944df65ee88
Dylan Neve <dylanneve1@gmail.com>
2021-12-01 18:46:39 +0000

    Revert "universal9810: Build required a2dp packages"
    This reverts commit c5c61bd36d12d167aea27e7486ad8b545659860f.

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -119,9 +119,7 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    audio.a2dp.default \
-    android.hardware.bluetooth.a2dp@1.0-impl \
-    android.hardware.bluetooth.a2dp@1.0-service
+    audio.a2dp.default
 
 # Camera
 PRODUCT_PACKAGES += \
