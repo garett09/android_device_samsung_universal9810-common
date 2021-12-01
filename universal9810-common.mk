907971dbe2cb9740a7d0ca17c85cfb2793d4c524
Dylan Neve <dylanneve1@gmail.com>
2021-12-01 20:09:28 +0000

    universal9810: Build libprotobuf-cpp-lite and it's vendorcompat
    * CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.drm@1.2-service.widevine": library "libprotobuf-cpp-lite.so" not found: needed by /vendor/lib/libwvhidl.so in namespace (default)

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -248,6 +248,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/configs/seccomp/mediaextractor_sec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor_sec.policy
 
+# libprotobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-lite \
+    libprotobuf-cpp-lite-vendorcompat
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0 \
